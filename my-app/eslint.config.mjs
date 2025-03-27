5208da97ed2d8f8a391933b2da1ff06eb59d16c8
Phemon420 <113652475+Phemon420@users.noreply.github.com>
2025-03-27 19:33:18 +0530

    'second commit'

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -3,10 +3,22 @@ import globals from "globals";
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+    },
+  },
 ]);
