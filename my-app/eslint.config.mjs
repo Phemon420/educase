f4b874b0fe2f02e0d71656965a4c97362e15eda4
Phemon420 <113652475+Phemon420@users.noreply.github.com>
2025-03-27 19:37:59 +0530

    'third'

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -2,23 +2,42 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,jsx,mjs,cjs}"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      parser: "@babel/eslint-parser",
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
       globals: {
         ...globals.browser,
       },
     },
     plugins: {
       react: pluginReact,
+      "react-hooks": pluginReactHooks,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...pluginReact.configs.flat.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
     },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    
   },
 ]);
